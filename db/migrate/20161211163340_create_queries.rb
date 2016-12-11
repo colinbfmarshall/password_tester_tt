3c453bb7e014b4b5cc41a0ce1c04254124f7570c
colinbfmarshall <colinbfmarshall@gmail.com>
2016-12-11 16:45:28 +0000

    add query model with password and result fields

---
+++ db/migrate/20161211163340_create_queries.rb
@@ -0,0 +1,10 @@
+class CreateQueries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :queries do |t|
+      t.string :password
+      t.string :result
+
+      t.timestamps
+    end
+  end
+end
