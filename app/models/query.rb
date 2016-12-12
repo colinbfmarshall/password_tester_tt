70631f285005154421a14cd98232a8c25c393b18
colinbfmarshall <colinbfmarshall@gmail.com>
2016-12-12 09:44:49 +0000

    updated method to work out how complex a password is

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,28 +1,30 @@
 class Query < ApplicationRecord
-validates :password, presence: true
-after_save :lets_try
+  validates :password, presence: true
+  after_save :lets_try
 
-    def lets_try
+  def lets_try
+    if self.result.blank?
+      lower_case = ('a'..'z').to_a
+      upper_case = lower_case.map(&:upcase)
+      numbers = ('0'..'9').to_a
+      #using q instead of quotation mark
+      characters = "!@#$%^&*()-_=+[{ ]};:'\|`~,<.>/?q ".chars.to_a 
 
-      if self.result.blank?
-        
-        lower_case = ('a'..'z').to_a
-        upper_case = lower_case.map(&:upcase)
-        numbers = ('0'..'9').to_a
-        #using q instead of quotation mark
-        characters = "!@#$%^&*()-_=+[{ ]};:'\|`~,<.>/?q ".chars.to_a
-        everything = lower_case + upper_case + numbers + characters
+      options = [lower_case, upper_case, numbers, characters]
 
-        total_guesses = everything.count ** password.chars.count
-
-        billion = 1000 ** 3 
-        ten_billion = billion * 10
-
-        how_long_in_seconds = total_guesses.to_f / ten_billion
-
-        self.update_attribute(:result, how_long_in_seconds)
+      options.reject! do |option|
+        (self.password.chars & option).empty?
       end
+      
+      total_guesses = options.flatten.count ** password.chars.count
 
+      billion = 1000 ** 3 
+      ten_billion = billion * 10
+
+      how_long_in_seconds = total_guesses.to_f / ten_billion
+
+      self.update_attribute(:result, how_long_in_seconds)
+      end
     end
 
 end
